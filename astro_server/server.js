8552e5e8b5c50a251fffab68fc5c0e91a6a7df0d
zflint <32756823+jzfcoder@users.noreply.github.com>
2022-07-24 20:27:33 -0700

    nineth commit

--- astro_server/server.js
+++ astro_server/server.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 8080;
 
-app.use(express());
+app.use(express.static());
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
