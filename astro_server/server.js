ac7063fe839aba7d10afe71debecd23ffee958fb
zflint <32756823+jzfcoder@users.noreply.github.com>
2022-07-24 20:50:49 -0700

    first commit

--- astro_server/server.js
+++ astro_server/server.js
@@ -1,9 +1,15 @@
 const express = require('express');
-
 const app = express();
+const path = require('path');
+
 const PORT = process.env.PORT || 8080;
 
-app.use(express());
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static('../astro_client/build'));
+    app.get('*', (req, res) => {
+        req.sendFile(path.resolve('astro_client/build/index.html', { root: "../" }));
+    })
+}
 
 app.listen(PORT, console.log(`Server started on port ${PORT}`));
 
